f7e37fbdfb289129a34339b98eb882d047ca10fe
Duy <duynv1895@gmail.com>
2021-10-02 01:11:08 +0700

    He

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trangmun';
+  isShowResult = false;
+  fileToUpload: File | null = null;
+
+  uploadFile() {
+    this.isShowResult = true;
+  }
+
+  uploadFiles(files: File){
+
+  } 
 }
